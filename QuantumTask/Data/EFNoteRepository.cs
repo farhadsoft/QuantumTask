b52a1d99083447b442e3a1dbddccdd4f7f68a7f2
Farhad Mammadov <dev@farhad.su>
2021-08-13 19:51:50 +0300

    Show/Hide button functionality

--- QuantumTask/Data/EFNoteRepository.cs
+++ QuantumTask/Data/EFNoteRepository.cs
@@ -9,12 +9,25 @@ namespace QuantumTask.Data
         {
             this.context = context;
         }
+        /// <summary>
+        /// Get all notes from data storage.
+        /// </summary>
         public IQueryable<Note> Notes => context.Notes;
+
+        /// <summary>
+        /// Create a new note.
+        /// </summary>
+        /// <param name="note">New note</param>
         public void Create(Note note)
         {
             context.Add(note);
             context.SaveChanges();
         }
+
+        /// <summary>
+        /// Edit a selected note.
+        /// </summary>
+        /// <param name="note">Selected note</param>
         public void Edit(Note note)
         {
             context.Update(note);
