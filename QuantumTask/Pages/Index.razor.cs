0f57127432f86b499ea2b7a88f0e6f8c56558a50
Farhad Mammadov <dev@farhad.su>
2021-08-14 11:37:05 +0300

    Create component for Show/Edit

---
+++ QuantumTask/Pages/Index.razor.cs
@@ -0,0 +1,57 @@
+﻿using QuantumTask.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantumTask.Pages
+{
+    public partial class Index
+    {
+        private IEnumerable<Note> Notes;
+
+        protected Note note = new Note();
+        protected int? editId = default;
+        protected bool edit = false;
+        protected string SearchTerm { get; set; } = "";
+
+        protected override async Task OnInitializedAsync()
+        {
+            Notes = await Task.Run(() => noteRepository.Notes.Where(x => x.NoteText.Contains(SearchTerm) || x.Title.Contains(SearchTerm)));
+        }
+
+        protected override Task OnParametersSetAsync()
+        {
+            return base.OnParametersSetAsync();
+        }
+
+        protected void Create()
+        {
+            note.Created = DateTime.Now;
+            noteRepository.Create(note);
+            note = new Note();
+        }
+
+        protected void Edit(Note note)
+        {
+            noteRepository.Edit(note);
+        }
+
+        protected void ShowEditForm()
+        {
+            edit = !edit;
+        }
+
+        protected void ShowEditor(int id)
+        {
+            if (editId == id)
+            {
+                editId = default;
+            }
+            else
+            {
+                editId = id;
+            }
+        }
+    }
+}
